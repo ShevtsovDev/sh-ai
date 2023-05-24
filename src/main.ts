04e7e20ff69534e1697e16352c1dfc737587137e
vladislavsevcov <eyeseeices@gmail.com>
2023-05-24 16:37:43 +0300

    add volume

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3001',
+      origin: 'https://dev.sellershub.ru/',
     },
   });
   await app.listen(3000);
